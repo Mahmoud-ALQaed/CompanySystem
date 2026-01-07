60a53998e1fbe5bed0d4f143e3e2b785be16c8a7
Mahmoud-ALQaed <mahmoudalqaed0@gmail.com>
2026-01-07 23:10:36 +0200

    Add utility functions for input validation and update pause duration

--- utils.py
+++ utils.py
@@ -1,18 +1,29 @@
 import time
 
-
 def pause():
-    time.sleep(8)
-
-
-def invalid_type():
-    print("Invalid data type")
-    pause()
-
+    time.sleep(5)
 
 def validate_positive_int(value):
     if not value.isdigit():
         return False
     if int(value) <= 0:
+        return False
+    return True
+
+def validate_name(value):
+    if value:
+        return True
+
+def validate_age(value):
+    if not value.isdigit():
+        return False
+    if int(value) < 18:
+        return False
+    return True
+
+def validate_bonus(value):
+    if not value.isdigit():
+        return False
+    if int(value) < 0:
         return False
     return True
